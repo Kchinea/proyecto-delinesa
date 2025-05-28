e0aeab03b240e5f5e922d84d8962be7c49343bc6
Kyliam Chinea <alu0101548050@ull.edu.es>
2025-05-28 14:18:12 +0000

    refactor

--- src/menu.js
+++ src/menu.js
@@ -16,16 +16,18 @@ function cargarMenu() {
         if (!menuList)
             return;
         menuList.innerHTML = platos.map((plato) => `
-    <div class="card mb-4" style="max-width: 500px; margin: 2rem auto;">
-      <div class="card-image">
-        <figure class="image is-4by3">
-          <img src="${plato.imagen}" alt="${plato.nombre}">
-        </figure>
-      </div>
-      <div class="card-content">
-        <p class="title is-4 has-text-delinesa-green">${plato.nombre}</p>
-        <p class="subtitle is-6">${plato.descripcion}</p>
-        <p class="has-text-weight-bold has-text-delinesa-orange">€${plato.precio.toFixed(2)}</p>
+    <div class="column is-12-mobile is-6-tablet is-4-desktop">
+      <div class="card mb-4">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="${plato.imagen}" alt="${plato.nombre}">
+          </figure>
+        </div>
+        <div class="card-content">
+          <p class="title is-4 has-text-delinesa-green">${plato.nombre}</p>
+          <p class="subtitle is-6">${plato.descripcion}</p>
+          <p class="has-text-weight-bold has-text-delinesa-orange">€${plato.precio.toFixed(2)}</p>
+        </div>
       </div>
     </div>
   `).join('');
